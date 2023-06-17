a813bb68f179a13b20aef98cb8df29548fb241c2
Evgeny Grin <cadair77@gmaile.com>
2023-06-17 11:40:56 +0300

    expenses list search capability

--- src/components/Expenses/expenses.tsx
+++ src/components/Expenses/expenses.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useRef } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { HomeViewContext } from '../../contexts/homeViewContext';
 import '../../App.css';
 import ImageGraph from '../../assets/graph.svg';
@@ -7,6 +7,9 @@ import { ExpensesList } from './expensesList';
 import { log } from 'console';
 import { logDOM } from '@testing-library/react';
 import { Expense } from '../../types';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 export const Expenses = () => {
 
@@ -15,22 +18,34 @@ export const Expenses = () => {
   //Work-around to avoid calling the callback in useEffect twise.
   //It happens because of the way React 18 renders components in development mode, in strict mode.
   //Solution from here: https://stackoverflow.com/questions/72406486/react-fetch-api-being-called-2-times-on-page-load
-  const renderAfterCalled = useRef(false);
+  // const renderAfterCalled = useRef(false);
 
-  useEffect(() => {
-    if (!renderAfterCalled.current) {
-      fetchData();
-    }
-    renderAfterCalled.current = true;
-  }, []);
+  // useEffect(() => {
+  //   if (!renderAfterCalled.current) {
+  //     fetchData();
+  //   }
+  //   renderAfterCalled.current = true;
+  // }, []);
 
 
-  const expenses = useMemo(() => {
-    const exp = extractExpenses();
-    console.log('Expenses: ', exp);
-    return exp;
-  }, [data]);
+  // const expenses = useMemo(() => {
+  //   const exp = extractExpenses();
+  //   console.log('Expenses: ', exp);
+  //   return exp;
+  // }, [data]);
 
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filterExpenses = (expenses: Expense[]) => { //TODO use type
+    return expenses.filter((expense) =>
+      expense.Name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
+
+  const filteredExpenses = useMemo(() => filterExpenses(extractExpenses()), [
+    data,
+    searchQuery,
+  ]);
   
   if (error) {
     return <div>{error || 'Error occured'}</div>
@@ -38,13 +53,30 @@ export const Expenses = () => {
 
   return (
     <>
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      marginTop: '16px',
-    }}>Expenses</div>
-      <ExpensesList expenses={expenses}/>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: '16px',
+        justifyContent: 'flex-start',
+        paddingLeft: '16px'
+      }}>
+        <TextField
+        label="Search"
+        value={searchQuery}
+        size="small"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      </div>
+      <ExpensesList expenses={filteredExpenses}/>
     </>
   );
 }
